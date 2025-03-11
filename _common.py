79157c6af94b96906ec7c68db6642544aabf779c
Aranaur <ihor.miroshnychenko@kneu.ua>
2025-03-12 00:49:31 +0200

    add text

--- _common.py
+++ _common.py
@@ -17,3 +17,24 @@ sns.set_style("whitegrid")
 palette = sns.color_palette("Set2")
 sns.set_palette(palette)
 sns.set_context("talk")
+
+# matplotlib settings
+plt.rcParams['font.size'] = 12
+plt.rcParams['axes.labelsize'] = 12
+plt.rcParams['axes.titlesize'] = 12
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['figure.titlesize'] = 12
+
+# Define colors
+red_pink   = "#e64173"
+turquoise  = "#20B2AA"
+orange     = "#FFA500"
+red        = "#fb6107"
+blue       = "#181485"
+navy       = "#150E37FF"
+green      = "#8bb174"
+yellow     = "#D8BD44"
+purple     = "#6A5ACD"
+slate      = "#314f4f"
