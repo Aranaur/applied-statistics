f4f107504b672276a39071624024a73667fb642c
Aranaur <ihor.miroshnychenko@kneu.ua>
2025-04-02 02:26:53 +0300

    add text

--- _common.py
+++ _common.py
@@ -2,6 +2,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import random
+import time
+from statsmodels.stats.proportion import proportion_confint
 
 from scipy.stats import (
     norm,
